c6cbbbbbf0b7f6438f32e5585b0ed214875bcd0d
alexdilion <alexdilion@protonmail.com>
2023-11-20 22:23:37 +0000

    Clean up task-summary-2 solution

--- week-9/lab-a/task-summary-2.py
+++ week-9/lab-a/task-summary-2.py
@@ -3,30 +3,23 @@
 import sys
 
 tasks = sys.stdin.read().split()
-i = 0
-while i < len(tasks) // 2:
-    tmp = tasks[i]
-    tasks[i] = tasks[len(tasks) - i - 1]
-    tasks[len(tasks) - i - 1] = tmp
-
-    i += 1
-
-user_data = {}
+data = {}
 i = 0
 while i < len(tasks):
-    user = tasks[i].split("/")[0]
-    task = tasks[i].split("/")[1].split(".")[0] + ".py"
-    correct = tasks[i].split(".")[2] == "correct"
+    ri = len(tasks) - i - 1
+    user = tasks[ri].split("/")[0]
+    task = tasks[ri].split("/")[1].split(".")[0] + ".py"
+    correct = tasks[ri].split(".")[2] == "correct"
 
-    if user not in user_data:
-        user_data[user] = {"correct": {}, "incorrect": {}}
+    if user not in data:
+        data[user] = {"correct": {}, "incorrect": {}}
 
-    if task not in user_data[user]["incorrect"] and correct:
-        user_data[user]["correct"][task] = 1
-    elif task not in user_data[user]["correct"] and not correct:
-        user_data[user]["incorrect"][task] = 1
+    if task not in data[user]["incorrect"] and correct:
+        data[user]["correct"][task] = 1
+    elif task not in data[user]["correct"] and not correct:
+        data[user]["incorrect"][task] = 1
 
     i += 1
 
-for user in user_data:
-    print(user, len(user_data[user]["correct"]))
+for user in data:
+    print(user, len(data[user]["correct"]))
