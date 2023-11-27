656e97c0f4c49a1de7e0adaa85a3b90840cbe163
alexdilion <alexdilion@protonmail.com>
2023-11-27 18:59:50 +0000

    Update plot-points

--- week-9/lab-b/plot-points.py
+++ week-9/lab-b/plot-points.py
@@ -3,24 +3,25 @@
 import sys
 
 points = sys.stdin.readlines()
+n = 20
 
 lines = []
 i = 0
-while i < 20:
-    lines.append([" "] * 20)
+while i < n:
+    lines.append([" "] * n)
     i += 1
 
 i = 0
 while i < len(points):
     xy = points[i].split()
     lines[int(xy[1])][int(xy[0])] = "*"
-
     i += 1
 
-print(" " + "-" * 20)
+print(" " + "-" * n)
+
 i = 0
 while i < len(lines):
     print("|" + "".join(lines[len(lines) - i - 1]) + "|")
     i += 1
 
-print(" " + "-" * 20)
+print(" " + "-" * n)
